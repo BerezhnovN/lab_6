98cde6f312ad31867e4ff4cc5bb023037b0423af
Nikita Berezhnov <nikita.berezhnov@wb.ru>
2022-12-24 23:48:17 +0300

    fix

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,26 +1,15 @@
 import { DataSourceOptions, DataSource } from 'typeorm';
 
-// export const dataSourceOption: DataSourceOptions = {
-//   type: 'postgres',
-//   host: process.env.DATABASE_HOST,
-//   database: process.env.DATABASE_NAME,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   entities: ['dist/**/**/*.entity.js'],
-//   migrations: ['dist/db/migrations/*.js'],
-//   synchronize: true,
-// }
-
 export const dataSourceOption: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  port: 5432,
-  username: 'postgres',
-  password: 'nik2701',
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   entities: ['dist/**/**/*.entity.js'],
-  migrations: ['dist/db/migrations/*.js'], 
-};
+  migrations: ['dist/db/migrations/*.js'],
+  synchronize: true,
+}
 
 const dataSource = new DataSource(dataSourceOption);
 export default dataSource;
